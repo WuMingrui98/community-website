31cf1b883ba101b41fe8b838586b071f118ef7f6
wmr <771478063@qq.com>
2021-12-30 14:07:34 +0800

    initialize project

---
+++ src/test/java/com/wmr/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmr.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
