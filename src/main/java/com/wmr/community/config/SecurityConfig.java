8a60c40d73db6e135f2a003d2441d55088280227
wmr <771478063@qq.com>
2022-01-20 16:23:20 +0800

    实现权限控制（补充）

---
+++ src/main/java/com/wmr/community/config/SecurityConfig.java
@@ -0,0 +1,98 @@
+package com.wmr.community.config;
+
+import com.wmr.community.util.CommunityConstant;
+import com.wmr.community.util.CommunityUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Configuration
+// Spring Security只做用户权限控制，登录登出的功能还都采用原来设计的逻辑
+// Spring Security需要获取我们登录后的权限，才能帮我们做权限控制
+// 在LoginTicketInterceptor中将授权信息存入到Spring Security
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 排除静态资源
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/password",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/unfollow"
+                ).hasAnyAuthority(
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR,
+                        AUTHORITY_USER
+                )
+                .anyRequest().permitAll();
+
+        // 手动关闭CSRF攻击配置
+        http.csrf().disable();
+
+        // 权限不够时的处理
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    // 没有登录，一种是同步的，一种是异步的
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        // 判断是否是Ajax请求
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        // 是Ajax请求，返回JSON格式的字符串
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            // 是string类型，前端需要解析
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "您还没有登录!"));
+                        } else {
+                            response.sendRedirect(request.getContextPath() + "/login");
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    // 权限控制，也需要判断是否是异步请求
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        // 判断是否是Ajax请求
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        // 是Ajax请求，返回JSON格式的字符串
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            // 是string类型，前端需要解析
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "您没有访问此功能的权限!"));
+                        } else {
+                            response.sendRedirect(request.getContextPath() + "/denied");
+                        }
+                    }
+                });
+
+
+        // Security底层默认会拦截/logout请求,进行退出处理.
+        // 覆盖它默认的逻辑,才能执行我们自己的退出代码
+        http.logout().logoutUrl("/securityLogout");
+    }
+}
